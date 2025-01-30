9801b621e7c32d47ef393239d09bb120d7bb9cb3
jlee1028 <jasonlee1028@yahoo.com>
2025-01-29 19:48:38 -0800

    debugging

--- convert_docx.py
+++ convert_docx.py
@@ -14,10 +14,13 @@ os.chdir(path)
 print(f"Current directory after cd DDIA: {os.getcwd()}")
 
 files = os.listdir()
+
 docx_files = []
 for file in files:
     if file.endswith('.docx'):
         docx_files.append(file[:-5])
+
+print(f'docs files: {docx_files}')
 
 for filename in docx_files:
     command = ['pandoc',
@@ -25,5 +28,7 @@ for filename in docx_files:
     f"--extract-media='media/{filename}'",
     f'{filename}.docx',
     '-o', f'{filename}.md']
+
+    print(f'command: {command}')
 
     subprocess.run(command, check=True)
